ffa3ffe5af50e28afe5b4bcc70bbaa9a853fa331
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2026-02-20 19:24:37 +0200

    changed nonChenged code below

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,10 @@ export const App = () => {
 
   // DON'T change the code below
   const increase = () => {
+    addOne();
     if (count % 5 === 0) {
       add100();
     }
-
-    addOne();
   };
 
   return (
